42928e863063a2e23a115c2a85d347d03231ced2
Vladimir Golub <golubvlauri@gmail.com>
2022-11-04 23:37:26 +0200

    add PZMenu

--- frontend/components/layouts/main/PZMenu.tsx
+++ frontend/components/layouts/main/PZMenu.tsx
@@ -1,26 +1,40 @@
 import PZLink from "../../ui/PZLink";
 import classes from '../../../styles/layouts/main/menu.module.scss'
 
-const PZMenu = () => {
+export interface IPZMenu {
+  className: string;
+}
+
+const PZMenu = ({ className }: IPZMenu) => {
   const pzLinks = [
     {
+      id: 1,
       title: 'Pizzas',
       href: '/pizzas'
     },
     {
+      id: 2,
       title: 'Snacks',
       href: '/snacks'
     },
     {
+      id: 3,
       title: 'Drinks',
       href: '/drinks'
     },
   ]
 
-
   return (
-    <nav className={ classes['menu'] }>
-      { pzLinks.map(item => (<PZLink className={ classes['menu__item'] } href={item['href']}>{ item['title'] }</PZLink>)) }
+    <nav className={ [classes['menu'], className].join(' ') }>
+      { pzLinks.map(item => (
+        <PZLink
+          key={item['id']}
+          className={ classes['menu__item'] }
+          href={item['href']}
+        >
+          { item['title'] }
+        </PZLink>
+      )) }
     </nav>
   )
 }
