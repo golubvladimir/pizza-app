a2b2073089c1f8b6e2d841ca1115409cd1de0da9
Vladimir Golub <golubvlauri@gmail.com>
2022-11-26 14:37:11 +0200

    add database config to app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,35 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    /**
+     * @nestjs/config config
+     */
     ConfigModule.forRoot({
       envFilePath: `${ process.cwd() }/.development.env`,
     }),
+
+    /**
+     * @nestjs/typeorm config
+     */
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [
+          `${ __dirname }/**/*.entity.{js|ts}`
+        ],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    })
   ],
 })
 export class AppModule {}
