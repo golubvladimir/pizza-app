1ce6507858dd688a645b87b81db341c1311c8f7c
Vladimir Golub <golubvlauri@gmail.com>
2022-11-26 23:53:31 +0200

    add

--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -4,8 +4,12 @@ import { SeederOptions } from 'typeorm-extension';
 const options: DataSourceOptions & SeederOptions = {
   type: 'mysql',
   database: 'pizza_db',
-  seeds: [`${ __dirname }/seeds/**/*.js`],
-  factories: [`${ __dirname }/factories/**/*.js`]
+  port: 8889,
+  host: 'localhost',
+  password: 'root',
+  username: 'root',
+  seeds: [`${ __dirname }/seeds/**/*{.js,.ts}`],
+  factories: [`${ __dirname }/factories/**/*{.js,.ts}`]
 };
 
 export const dataSource = new DataSource(options);
