b7fdfda57be0f0804890fb1b823930a5d896d7a1
Vladimir Golub <golubvlauri@gmail.com>
2022-11-26 11:56:43 +0200

    add @nestjs/config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${ process.cwd() }/.development.env`,
+    }),
+  ],
 })
 export class AppModule {}
