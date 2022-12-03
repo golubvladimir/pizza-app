fa4622bd22a9a2cba856be46a9e04ccc9b9f1809
Vladimir Golub <golubvlauri@gmail.com>
2022-12-03 16:26:58 +0200

    add user seeder

---
+++ backend/src/database/seeds/user.seeder.ts
@@ -0,0 +1,15 @@
+import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+import { DataSource } from 'typeorm';
+import { UserEntity } from '../../users/entities/user.entity';
+
+export default class ProductSeeder implements Seeder {
+  public async run(
+    dataSource: DataSource,
+    factoryManager: SeederFactoryManager
+  ): Promise<any> {
+    console.log('user seed');
+
+    const userFactory = await factoryManager.get(UserEntity);
+    await userFactory.save();
+  }
+}
