ee0ea35a153b5e5a7424e2aad322ac0ed2ef1f8c
Vladimir Golub <golubvlauri@gmail.com>
2022-12-03 16:24:28 +0200

    add user factory

---
+++ backend/src/database/factories/user.factory.ts
@@ -0,0 +1,13 @@
+import { setSeederFactory, useSeederFactory } from 'typeorm-extension';
+import { UserEntity } from '../../users/entities/user.entity';
+
+export default setSeederFactory(UserEntity, async (faker) => {
+  console.log('user _factory');
+
+  const user = new UserEntity();
+
+  user.username = faker.name.firstName().toLowerCase();
+  user.password = '12345678';
+
+  return user;
+})
